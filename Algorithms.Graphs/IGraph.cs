718b57a81055d9bf3985c6893dd13fb30890f6f9
Vaibhav Bali <vbali@hub24.com.au>
2019-01-07 13:37:56 +1100

    init

---
+++ Algorithms.Graphs/IGraph.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Graphs
+{
+    public interface IGraph
+    {
+        int NumberOfVertices { get; }
+        int NumberOfEdges { get; }
+        void AddEdge(int start, int end, int weight);
+        void AddVertex();
+        int GetWeight(int start, int end);
+        bool IsDirected { get; }
+    }
+}
