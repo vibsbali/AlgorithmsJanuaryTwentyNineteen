5370aee8b9eb3b44f0eefa7feab537f10d0289a7
Vaibhav Bali <vbali@hub24.com.au>
2019-01-08 17:30:19 +1100

    Added tests for AdjacencyList

--- Algorithms.GraphTests/AdjacencyListTests.cs
+++ Algorithms.GraphTests/AdjacencyListTests.cs
@@ -1,4 +1,5 @@
-﻿using Algorithms.Graphs;
+﻿using System;
+using Algorithms.Graphs;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Algorithms.GraphTests
@@ -6,62 +7,89 @@ namespace Algorithms.GraphTests
     [TestClass]
     public class AdjacencyListTests
     {
-        IGraph UndirectedGraph;
-        IGraph DirectedGraph;
+        private IGraph _undirectedGraph;
+        IGraph _directedGraph;
 
         [TestInitialize]
         public void Initialise()
         {
-            UndirectedGraph = new AdjacencyList(10, false);
-            DirectedGraph = new AdjacencyList(10, true);
+            _undirectedGraph = new AdjacencyList(10, false);
+            _directedGraph = new AdjacencyList(10, true);
         }
 
         [TestMethod]
         public void Create_AdjacencyMatrix_With_Ten_Vertices_Assert_Number_Of_Vertices_Equal_Ten()
         {
-            var numberOfVertices = UndirectedGraph.NumberOfVertices;
-            Assert.AreEqual(10, numberOfVertices);
-            
+            Assert.AreEqual(10, _undirectedGraph.NumberOfVertices);
+            Assert.AreEqual(10, _directedGraph.NumberOfVertices);
+        }
+
+        [TestMethod]
+        public void Add_One_More_Vertex()
+        {
+            _undirectedGraph.AddVertex();
+            _directedGraph.AddVertex();
+
+            Assert.AreEqual(11, _directedGraph.NumberOfVertices);
+            Assert.AreEqual(11, _undirectedGraph.NumberOfVertices);
+        }
+
+        [TestMethod]
+        public void Default_Initialization_Zero_Edges()
+        {
+            Assert.AreEqual(0, _undirectedGraph.NumberOfEdges);
+            Assert.AreEqual(0, _directedGraph.NumberOfEdges);
         }
 
 
         [TestMethod]
-        public void Add_Edge_Between_Zero_And_One_Assert_Are_Neighbours()
+        public void Add_Edge_Between_Zero_And_One_Assert_Are_Neighbours_And_Edge_Count2ForUndirected_CountOf1ForDirected()
         {
-            UndirectedGraph.AddEdge(0, 1, 0);
-            var neighboursOfZero = UndirectedGraph.GetReachableNeighbours(0);
-            var neighboursOfOne = UndirectedGraph.GetReachableNeighbours(1);
+            _undirectedGraph.AddEdge(0, 1);
+            var neighboursOfZero = _undirectedGraph.GetReachableNeighbours(0);
+            var neighboursOfOne = _undirectedGraph.GetReachableNeighbours(1);
 
             Assert.IsTrue(neighboursOfZero.Contains(1));
             Assert.IsTrue(neighboursOfOne.Contains(0));
+            Assert.AreEqual(2, _undirectedGraph.NumberOfEdges);
 
-            DirectedGraph.AddEdge(0, 1, 0);
-            neighboursOfZero = DirectedGraph.GetReachableNeighbours(0);
-            neighboursOfOne = DirectedGraph.GetReachableNeighbours(1);
+            _directedGraph.AddEdge(0, 1);
+            neighboursOfZero = _directedGraph.GetReachableNeighbours(0);
+            neighboursOfOne = _directedGraph.GetReachableNeighbours(1);
 
             Assert.IsTrue(neighboursOfZero.Contains(1));
             Assert.IsFalse(neighboursOfOne.Contains(0));
+            Assert.AreEqual(1, _directedGraph.NumberOfEdges);
         }
 
         [TestMethod]
         public void Add_Edge_Between_Zero_And_One_With_Weight_Assert_Neighbours_With_Weight()
         {
-            UndirectedGraph.AddEdge(0, 1, 2);
-            var neighboursOfZero = UndirectedGraph.GetReachableNeighbours(0);
-            var neighboursOfOne = UndirectedGraph.GetReachableNeighbours(1);
+            _undirectedGraph.AddEdge(0, 1, 2);
+            var neighboursOfZero = _undirectedGraph.GetReachableNeighbours(0);
+            var neighboursOfOne = _undirectedGraph.GetReachableNeighbours(1);
 
             Assert.IsTrue(neighboursOfZero.Contains(1));
             Assert.IsTrue(neighboursOfOne.Contains(0));
-            Assert.AreEqual(2, UndirectedGraph.GetWeight(0, 1));
-            Assert.AreEqual(2, UndirectedGraph.GetWeight(1, 0));
+            Assert.AreEqual(2, _undirectedGraph.GetWeight(0, 1));
+            Assert.AreEqual(2, _undirectedGraph.GetWeight(1, 0));
 
-            DirectedGraph.AddEdge(0, 1, 2);
-            neighboursOfZero = DirectedGraph.GetReachableNeighbours(0);
-            neighboursOfOne = DirectedGraph.GetReachableNeighbours(1);
+            _directedGraph.AddEdge(0, 1, 2);
+            neighboursOfZero = _directedGraph.GetReachableNeighbours(0);
+            neighboursOfOne = _directedGraph.GetReachableNeighbours(1);
 
             Assert.IsTrue(neighboursOfZero.Contains(1));
             Assert.IsFalse(neighboursOfOne.Contains(0));
-            Assert.AreEqual(2, DirectedGraph.GetWeight(0, 1));
+            Assert.AreEqual(2, _directedGraph.GetWeight(0, 1));
+
+            Assert.ThrowsException<InvalidOperationException>(() => _directedGraph.GetWeight(1, 0));
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            _directedGraph = null;
+            _undirectedGraph = null;
         }
     }
 }
