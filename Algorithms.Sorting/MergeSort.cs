c98eca0a87d46eeb2654dbf4ebdc9a910ddbacb8
Vaibhav Bali <vbali@hub24.com.au>
2019-01-14 10:26:27 +1100

    Implemented MergeSort

--- Algorithms.Sorting/MergeSort.cs
+++ Algorithms.Sorting/MergeSort.cs
@@ -1,13 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Algorithms.Sorting
 {
-    public class MergeSort
+    public class MergeSort<T>
+           where T : IComparable<T>
     {
-        
+        public void Sort(T[] arrayToSort)
+        {
+            SortRecursively(arrayToSort);
+        }
+
+        private void SortRecursively(T[] arrayToSort)
+        {
+            if (arrayToSort == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            if (arrayToSort.Length == 1)
+            {
+                return;
+            }
+
+            var midPoint = arrayToSort.Length / 2;
+            T[] leftArray = new T[midPoint];
+            T[] rightArray = new T[arrayToSort.Length - midPoint];
+            Array.Copy(arrayToSort, 0, leftArray, 0, midPoint);
+            Array.Copy(arrayToSort, midPoint, rightArray, 0, rightArray.Length);
+            SortRecursively(leftArray);
+            SortRecursively(rightArray);
+
+            Merge(leftArray, rightArray, arrayToSort);
+        }
+
+        private void Merge(T[] leftArray, T[] rightArray, T[] arrayToSort)
+        {
+            var i = 0;
+            int j = 0;
+            int k = 0;
+
+            while (i != leftArray.Length && j != rightArray.Length)
+            {
+                if (i < leftArray.Length && leftArray[i].CompareTo(rightArray[j]) <= 0)
+                {
+                    arrayToSort[k++] = leftArray[i++];
+                }
+                else if (j < rightArray.Length && leftArray[i].CompareTo(rightArray[j]) > 0)
+                {
+                    arrayToSort[k++] = rightArray[j++];
+                }
+
+                if (i == leftArray.Length)
+                {
+                    while (j < rightArray.Length)
+                    {
+                        arrayToSort[k++] = rightArray[j++];
+                    }
+                }
+                else if (j == rightArray.Length)
+                {
+                    while (i < leftArray.Length)
+                    {
+                        arrayToSort[k++] = leftArray[i++];
+                    }
+                }
+            }
+        }
     }
 }
